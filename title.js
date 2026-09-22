68880a69f4c1f33d8516d36fdcc37e2ed1258055
J Schere <owner@jschere.com>
2026-09-22 14:41:52 -0400

    expanded logo, restructured title page

--- title.js
+++ title.js
@@ -3,6 +3,8 @@
 
 console.log("reborn at last")
 
+const eyeScale = 0.5;
+
 let mouseFollower = document.createElement("div");
 document.body.appendChild(mouseFollower);
 mouseFollower.id = "follower";
@@ -27,3 +29,31 @@ setInterval(() => {
 document.getElementById("enter").addEventListener("click", (ev) => {
     window.location.assign("/os/")
 })
+
+let eyes = 0;
+
+let eyeblink = (x, y, angle) => {
+    eyes++;
+    eyeImg = document.createElement("img");
+    eyeImg.src = "./assets/eye.png"
+    eyeImg.classList.add("blinkeye");
+    // these have already been put in the pixel unit
+    eyeImg.style.top = y; 
+    eyeImg.style.left = x;
+    eyeImg.style.transform = `scale(${eyeScale}) rotateZ(${angle})`;
+    document.body.appendChild(eyeImg);
+    void eyeImg.offsetWidth; // force a style recalculation
+    eyeImg.classList.add("trigger");
+    setTimeout(() => {
+        eyeImg.remove();
+        eyes--;
+    }, 3000)
+}
+
+setInterval( () => {
+    let eyeX = Math.floor(Math.random() * window.innerWidth);
+    let eyeY = Math.floor(Math.random() * window.innerHeight);
+    let randomAngle = Math.floor(Math.random() * 360);
+
+    eyeblink(eyeX + "px", eyeY + "px", randomAngle + "deg");
+}, eyes > 5 ? 1500 : 4000);
